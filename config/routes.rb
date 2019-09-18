96418ce30509105a3c1ba32d77fadac5e979e69b
EC2 Default User <ec2-user@ip-172-31-21-142.us-east-2.compute.internal>
2019-09-18 04:26:55 +0000

    注文管理ページの作成
    管理用の注文ページをつくるため

--- config/routes.rb
+++ config/routes.rb
@@ -48,6 +48,7 @@ Rails.application.routes.draw do
       get "search", on: :collection
     end
     resources :items
+    resources :orders
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
